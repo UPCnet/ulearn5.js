d0993da19eaa0e197d97f4348a9ecc26732ef276
Pilar Marinas <pilar.marinas@upcnet.es>
2019-04-08 09:19:55 +0200

    Back to development: 0.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.11'
+version = '0.12.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
