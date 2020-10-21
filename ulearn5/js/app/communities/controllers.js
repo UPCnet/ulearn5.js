31c1f76a6d87bf3c633566e7e07f93ec85d8ce57
Iago López Fernández <iago.lopez@upcnet.es>
2020-10-21 13:02:36 +0200

    Notificaciones push + grunt

--- ulearn5/js/app/communities/controllers.js
+++ ulearn5/js/app/communities/controllers.js
@@ -91,6 +91,30 @@ GenwebApp.controller('AllCommunities', ['_', 'plonePortalURL', 'CommunityInfo', 
             });
     };
 
+
+    self.toggleNotNotifyPush = function (community) {
+        $http({
+                method: 'POST',
+                url: community.url + '/toggle-notnotifypush',
+                headers: {
+                    'X-CSRF-TOKEN': CodeInfo.csrf_token
+                }
+            })
+            .then(function successCallback(response) {
+                community.not_notify_push = !community.not_notify_push;
+            },function errorCallback(response) {
+                $translate(['ALLCOMMUNITIES_VIEW.NOTIFYPUSHERROR'])
+                    .then(function (translations) {
+                        SweetAlert.swal({
+                            title: 'Error',
+                            description: translations['ALLCOMMUNITIES_VIEW.NOTIFYPUSHERROR'],
+                            type: 'error',
+                            timer: 2000
+                        });
+                    });
+            });
+    };
+
     self.subscribe = function (community) {
         $translate(['COMMUNITY_SUBSCRIBE.TITLE',
                 'COMMUNITY_SUBSCRIBE.SUCCESSBTN',
@@ -167,6 +191,7 @@ GenwebApp.controller('AllCommunities', ['_', 'plonePortalURL', 'CommunityInfo', 
                                 .then(function successCallback(response) {
                                     self.user_subscriptions.pop(community.url);
                                     community.favorited = false;
+                                    community.not_notify_push = false;
                                     SweetAlert.swal({
                                         title: translations['COMMUNITY_UNSUBSCRIBE.DONE'],
                                         type: 'success',
